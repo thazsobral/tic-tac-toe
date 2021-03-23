188cec3ba278f1d4369705df4f1cfb4b5164bc7a
thazsobral <thazsobral@gmail.com>
2021-03-23 00:16:25 -0300

    refactored code

--- script.js
+++ script.js
@@ -1,48 +1,55 @@
 const players = ['x', 'o']
 var currentPlayer
 var labelCurrentPlayer
+var boxes
 
-main()
+main ()
 
 function main() {
   currentPlayer = players[0]
   labelCurrentPlayer = document.getElementById('player')
-  replaceLabelPlayer(currentPlayer)
-}
+  boxes = document.getElementsByClassName('box')
 
-function selectPosition(id) {
-  markPosition(id, currentPlayer)
-  verifyWinner()
-  currentPlayer = changePlayer(currentPlayer)
-  replaceLabelPlayer(currentPlayer)
-}
-
-function markPosition(id, currentPlayer) {
-  let position = document.getElementById(id)
-  lockPosition(position)
-  position.innerHTML = currentPlayer
-}
-
-function lockPosition(position) {
-  position.removeAttribute('onClick')
-  position.classList.add('disable-block')
-}
-
-function changePlayer(currentPlayer) {
-  let indexPlayer = players.indexOf(currentPlayer)
-  if(indexPlayer == players.length-1) {
-    return currentPlayer = players[0]
-  } else {
-    return currentPlayer = players[indexPlayer + 1]
+  for (const key in boxes) {
+    boxes[key].innerHTML = ''
+    boxes[key].setAttribute('class', 'box box-active')
+    boxes[key].setAttribute('onClick', 'selectionPosition(this.id)')
   }
 }
 
-function replaceLabelPlayer(currentPlayer) {
+function reset() {
+  main()
+}
+
+function selectionPosition(id) {
+  boxes[id].innerHTML = currentPlayer
+  var victoryValues = seekVictoryValues()
+  if(victoryValues == false) {
+    boxes[id].setAttribute('class', 'box block-box')
+    boxes[id].removeAttribute('onClick')
+    changePlayer()
+  } else {
+    victoryValues.forEach(el => {
+      boxes[el].setAttribute('class', 'box winner-box')
+    })
+    for(const key in boxes) {
+      boxes[key].removeAttribute('onClick')
+    }
+  }
+}
+
+function changePlayer() {
+  let indexPlayer = players.indexOf(currentPlayer)
+  if(indexPlayer == players.length-1) {
+    currentPlayer = players[0]
+  } else {
+    currentPlayer = players[indexPlayer + 1]
+  }
   labelCurrentPlayer.innerHTML = currentPlayer
 }
 
-function verifyWinner(){
-  /*Rules Winner:
+function seekVictoryValues() {
+  /*Rules Victory:
     [
       [1,2,3] = [0,1,2],
       [1,4,7] = [0,3,6],
@@ -54,68 +61,32 @@ function verifyWinner(){
       [7,8,9] = [6,7,8]
     ]
   */
-  let sequenceWinner
-  let blocks = getAllValuesBlocks()
+  let valuesBoxes = getValuesBoxes(boxes)
+  let sequenceVictory
+
   //Verify Horizontal
-  if((blocks[0] == blocks[1] && blocks[1] == blocks[2]) && blocks[0] == currentPlayer) sequenceWinner = [0,1,2]
-  if((blocks[3] == blocks[4] && blocks[4] == blocks[5]) && blocks[3] == currentPlayer) sequenceWinner = [3,4,5]
-  if((blocks[6] == blocks[7] && blocks[7] == blocks[8]) && blocks[6] == currentPlayer) sequenceWinner = [6,7,8]
+  if((valuesBoxes[0] == valuesBoxes[1] && valuesBoxes[1] == valuesBoxes[2]) && valuesBoxes[0] == currentPlayer) sequenceVictory = [0,1,2]
+  if((valuesBoxes[3] == valuesBoxes[4] && valuesBoxes[4] == valuesBoxes[5]) && valuesBoxes[3] == currentPlayer) sequenceVictory = [3,4,5]
+  if((valuesBoxes[6] == valuesBoxes[7] && valuesBoxes[7] == valuesBoxes[8]) && valuesBoxes[6] == currentPlayer) sequenceVictory = [6,7,8]
   //Verify Vertical
-  if((blocks[0] == blocks[3] && blocks[3] == blocks[6]) && blocks[0] == currentPlayer) sequenceWinner = [0,3,6]
-  if((blocks[1] == blocks[4] && blocks[4] == blocks[7]) && blocks[1] == currentPlayer) sequenceWinner = [1,4,7]
-  if((blocks[2] == blocks[5] && blocks[5] == blocks[8]) && blocks[2] == currentPlayer) sequenceWinner = [2,5,8]
+  if((valuesBoxes[0] == valuesBoxes[3] && valuesBoxes[3] == valuesBoxes[6]) && valuesBoxes[0] == currentPlayer) sequenceVictory = [0,3,6]
+  if((valuesBoxes[1] == valuesBoxes[4] && valuesBoxes[4] == valuesBoxes[7]) && valuesBoxes[1] == currentPlayer) sequenceVictory = [1,4,7]
+  if((valuesBoxes[2] == valuesBoxes[5] && valuesBoxes[5] == valuesBoxes[8]) && valuesBoxes[2] == currentPlayer) sequenceVictory = [2,5,8]
   //Verify Diagonal
-  if((blocks[0] == blocks[4] && blocks[4] == blocks[8]) && blocks[0] == currentPlayer) sequenceWinner = [0,4,8]
-  if((blocks[2] == blocks[4] && blocks[4] == blocks[6]) && blocks[2] == currentPlayer) sequenceWinner = [2,4,6]
+  if((valuesBoxes[0] == valuesBoxes[4] && valuesBoxes[4] == valuesBoxes[8]) && valuesBoxes[0] == currentPlayer) sequenceVictory = [0,4,8]
+  if((valuesBoxes[2] == valuesBoxes[4] && valuesBoxes[4] == valuesBoxes[6]) && valuesBoxes[2] == currentPlayer) sequenceVictory = [2,4,6]
   
-  if(sequenceWinner != undefined) {
-    markPositionWinner(sequenceWinner)
+  if(sequenceVictory == undefined) {
+    return false
+  } else {
+    return sequenceVictory
   }
 }
 
-function getAllValuesBlocks() {
-  let blocks = document.getElementsByClassName('block')
-  let valuesBlocks = []
-  for (const key in blocks) {
-    valuesBlocks.push(blocks[key].innerHTML)
+function getValuesBoxes(boxes) {
+  let valuesBoxes = []
+  for (const key in boxes) {
+    valuesBoxes.push(boxes[key].innerHTML)
   }
-  return valuesBlocks
-}
-
-function markPositionWinner(sequence) {
-  sequence.forEach(el => {
-    let block = document.getElementById(el)
-    block.removeAttribute('onclick')
-    block.classList.add('winner-block')
-    console.log(el)
-  })
-  disableBoard()
-  
-  // for (const key in sequence) {
-  //   let block = document.getElementById(key)
-  //   block.removeAttribute('onclick')
-  //   block.classList.add('winner-block')
-  //   console.log(key)
-  // }
-}
-
-function disableBoard() {
-  let blocks = document.getElementsByClassName('block')
-  for (const key in blocks) {
-    blocks[key].removeAttribute('onclick')
-  }
-}
-
-function resetGame() {
-  main()
-  resetBoard()
-}
-
-function resetBoard() {
-  let blocks = document.getElementsByClassName('block')
-  for (const key in blocks) {
-    blocks[key].innerHTML = ''
-    blocks[key].setAttribute('class', 'block')
-    blocks[key].setAttribute('onClick', 'selectPosition(this.id)')
-  }
+  return valuesBoxes
 }
